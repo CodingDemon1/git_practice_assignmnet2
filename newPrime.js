41c00c180e8409668c802127bd4c03fa7a26184d
Nitish Kumar <kumar.nit1021@gmail.com>
2022-12-08 23:03:34 +0530

    Prime_Problem

---
+++ newPrime.js
@@ -0,0 +1,8 @@
+//This is a program to find out a number if it is a prime or not
+
+let num =17;
+    for(let i=0;i<num;i++){
+        if(num%2==0){
+            console.log("True")
+        }
+    }
