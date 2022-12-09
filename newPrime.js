a6bdfd02ef2540435e909a4c98818f4367b28e73
SHUBHAM KUMAR <shubham.rajveer19@gmail.com>
2022-12-09 16:17:14 +0530

    code_fix

--- newPrime.js
+++ newPrime.js
@@ -1,8 +1,13 @@
 //This is a program to find out a number if it is a prime or not
 
 let num =17;
-    for(let i=0;i<num;i++){
-        if(num%2==0){
-            console.log("True")
-        }
+let count=0;
+    for(let i=1;i<=num;i++){
+        if(num%i==0){
+          count++
+    }
+    }if (count==2){
+      console.log("its a prime number")
+    }else{
+      console.log("its not a prime")
     }
